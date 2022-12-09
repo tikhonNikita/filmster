df2aadaa6b46ad4f081dfedeac5cadb327cf23af
Nikita Tikhonov <pofofwar@gmail.com>
2022-12-09 21:16:12 +0300

    update details layout

--- app/src/main/java/com/nikita/filmapp/viewModels/MovieDetailsViewModel.kt
+++ app/src/main/java/com/nikita/filmapp/viewModels/MovieDetailsViewModel.kt
@@ -9,6 +9,7 @@ import com.nikita.filmapp.repository.MoviesRepository
 import kotlinx.coroutines.launch
 
 //TODO: implement UI state (loading, error, detailedMovie)
+//TODO: Исправить тайтл для фрагмента с деталями, добавить кнопку лайка на тулбар слева сверху
 class MovieDetailsViewModel(private val repository: MoviesRepository) : ViewModel() {
     private val _detailedMovie = MutableLiveData<DetailedMovie>()
     val detailedMovie: LiveData<DetailedMovie>
