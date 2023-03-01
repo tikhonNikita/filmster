c9358d357f430dad04ef0941cf1cabead16af8a8
Nikita Tikhonov <pofofwar@gmail.com>
2023-03-01 13:41:44 +0300

    start work on swipe to delete

--- app/src/main/java/com/nikita/filmapp/fragments/FavouritesFragment.kt
+++ app/src/main/java/com/nikita/filmapp/fragments/FavouritesFragment.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.nikita.filmapp.MainActivity
 import com.nikita.filmapp.compose.favourites.FavouriteItem
 import com.nikita.filmapp.databinding.FavouritesFragmentBinding
@@ -42,14 +55,61 @@ class FavouritesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
+    @OptIn(ExperimentalMaterialApi::class)
     private fun showFavorites(movies: List<Movie>) {
         binding.favouritesList.setContent {
             LazyColumn(content = {
-                items(movies.size) { index ->
-                    FavouriteItem(
-                        title = movies[index].title,
-                        imageURL = IMG_URL + movies[index].poster,
-                        modifier = Modifier.padding(vertical = 8.dp)
+                items(movies.size, { index -> movies[index].id }) { index ->
+                    val dismissState = rememberDismissState(
+                        confirmStateChange = {
+                            if (it == DismissValue.DismissedToStart) {
+                                viewModel.removeMovieFromFavourites(movies[index])
+                            }
+                            true
+                        }
+                    )
+                    SwipeToDismiss(
+                        state = dismissState,
+                        directions = setOf(DismissDirection.EndToStart),
+                        dismissThresholds = { FractionalThreshold(0.5f) },
+                        background = {
+                            val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
+                            val color by animateColorAsState(
+                                targetValue = when (dismissState.targetValue) {
+                                    DismissValue.DismissedToStart -> MaterialTheme.colorScheme.error
+                                    else -> Color.Transparent
+                                }
+                            )
+
+                            val icon = when (direction) {
+                                DismissDirection.EndToStart -> Icons.Default.Delete
+                                else -> Icons.Default.Done
+                            }
+
+                            val scale by animateFloatAsState(targetValue = if (dismissState.targetValue == DismissValue.Default) 0.75f else 1.2f)
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .fillMaxHeight()
+                                    .background(color)
+                                    .padding(start = 12.dp, end = 12.dp)
+                            ) {
+                                Icon(
+                                    icon, contentDescription = null, modifier = Modifier
+                                        .align(Alignment.CenterEnd)
+                                        .scale(scale)
+                                )
+
+                            }
+
+                        },
+                        dismissContent = {
+                            FavouriteItem(
+                                title = movies[index].title,
+                                imageURL = IMG_URL + movies[index].poster,
+                                modifier = Modifier.padding(vertical = 8.dp)
+                            )
+                        }
                     )
                 }
             }, modifier = Modifier.padding(bottom = 72.dp))
